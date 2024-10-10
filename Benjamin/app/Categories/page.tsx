6a2bd3ae0e52f1cbac56764676b5c8838ffd8f0d
Shivansh <shivanshsaxena2910@gmail.com>
2024-10-10 20:13:59 +0530

    added categories page

---
+++ Benjamin/app/Categories/page.tsx
@@ -0,0 +1,24 @@
+import Stores from "@/components/stores";
+import { Button } from "@/components/ui/button";
+import SeperatorHeading from "@/components/ui/seperatorHeading";
+
+const Category = () => {
+  return (
+    <div className="">
+      <div className="h-full">
+        <div className="flex items-center justify-center bg-customTeal h-full mb-20 p-24">
+          <div className="text-7xl font-bold font-handlee">Our Categories</div>
+        </div>
+      </div>
+      {/* <SeperatorHeading label="Trending Deals"/> */}
+      <Stores />
+      <div className="flex items-center justify-center flex-col px-20">
+        <Button className="rounded-full bg-customTeal h-10 w-40 flex items-center justify-center text-lg m-10">
+          Load more
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
