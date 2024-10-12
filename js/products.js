8ad19bfaff6c78ea47a0afd165d02c5939b2ebd4
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-10-12 18:52:45 +0530

    fixes #331

--- js/products.js
+++ js/products.js
@@ -253,5 +253,11 @@ function loadMoreProducts() {
 }
 
 function addToCart(productName) {
-    alert(`${productName} added to cart!`);
+    const notification = document.getElementById('notification');
+    notification.innerText = `${productName} added to cart!`;
+    notification.style.display = 'block';
+
+    setTimeout(() => {
+        notification.style.display = 'none';
+    }, 3000); // Hide after 3 seconds
 }
