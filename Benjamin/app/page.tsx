4857584c130e02f3d3a4273562e019eb61a8b292
Bhumika <sharmabhmi@gmail.com>
2024-10-14 20:38:01 +0530

    Ui fixed

--- Benjamin/app/page.tsx
+++ Benjamin/app/page.tsx
@@ -8,13 +8,15 @@ import LearnAboutUs from "@/components/about us/learnAboutUs";
 export default function Home() {
   return (
     <div className="h-full bg-DarkGray">
-      <Hero/>
-      <SeperatorHeading label="Learn About us"/>
-      <LearnAboutUs />
-      <Features/>
-      <div className="w-full border-b border-Green my-10"/>
+      <Hero />
+      <SeperatorHeading label="Learn About us" />
 
-      <Stores/>
+      <LearnAboutUs />
+
+      <Features />
+      <div className="w-full border-b border-Green my-10" />
+
+      <Stores />
     </div>
   );
 }
