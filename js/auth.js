84129bf17f62f357d12d8594a44fda5a68428ca2
Shivansh Saxena <112249407+ShivanshPlays@users.noreply.github.com>
2024-10-14 21:25:01 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const authButton = document.getElementById('authButton');
   const currentUser = localStorage.getItem('currentUser');
 
-  // some random changes
+// Check if there's a logged-in user
+  if (currentUser) {
+      authButton.textContent = currentUser; // Change button text to username
+      authButton.href = "#"; // Optionally, change the link to something else or remove it
+      authButton.classList.add('btn-secondary'); // Optionally change button style
+
+    // Add event listener for logout
+    authButton.addEventListener('click', function () {
+      // Show confirmation dialog before logging out
+      const confirmLogout = confirm('Do you want to logout?');
+
       if (confirmLogout) {
           // If confirmed, remove the current user from localStorage
           localStorage.removeItem('currentUser');
