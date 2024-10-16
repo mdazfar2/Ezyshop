ceb89fca50876f6af35f3b6ab1d9f3da674f9419
Shivansh <shivanshsaxena2910@gmail.com>
2024-10-17 05:10:06 +0530

    added signup functionality

--- components/authButtons.tsx
+++ components/authButtons.tsx
@@ -1,23 +1,26 @@
 
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { signOut, useSession } from "next-auth/react";
 
 const AuthButtons = () => {
+
+  const session=useSession()
   return (
     <div className="flex items-center justify-center gap-2">
-      {/* {session.status == "unauthenticated" && ( */}
+      {session.status == "unauthenticated" && (
         <Link href={"/login"}>
         <Button size={"lg"} className={`bg-customTeal dark:bg-Green  hover:border rounded-xl`}>
           Login / Signup
         </Button>
       </Link>
-      {/* )}
+      )}
       {session.status == "authenticated" && (
         <>
           <div>Hi! {session.data.user?.name}</div>
           <Button className="rounded-full" variant={"outline"} onClick={() => signOut()}>Log out</Button>
         </>
-      )} */}
+      )}
     </div>
   );
 };
