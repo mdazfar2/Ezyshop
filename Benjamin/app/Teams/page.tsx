be694a9b0378536c6eabbfbe4aba0d1b0d2adc9e
Shivansh <shivanshsaxena2910@gmail.com>
2024-10-16 19:44:36 +0530

    added changes

--- Benjamin/app/Teams/page.tsx
+++ Benjamin/app/Teams/page.tsx
@@ -3,25 +3,27 @@ import UnderConstruction from "@/components/ui/underConstruction";
 
 const Teams = () => {
   return (
-    <div className="h-full pb-20  dark:bg-DarkGray">
-      <div className="h-full">
-        <div className="text-white flex items-center justify-center  bg-customTeal dark:bg-gradient-to-r from-Green to-Yellow h-full mb-20 p-24">
-          <div className="text-4xl pt-5 lg:pt-0 lg:text-7xl text-gray-200 text-center lg:text-start font-extrabold font-handlee">
-            Our Teams
-          </div>
-        </div>
-      </div>
+    <UnderConstruction/>
+    
+    // <div className="h-full pb-20  dark:bg-DarkGray">
+    //   <div className="h-full">
+    //     <div className="text-white flex items-center justify-center  bg-customTeal dark:bg-gradient-to-r from-Green to-Yellow h-full mb-20 p-24">
+    //       <div className="text-4xl pt-5 lg:pt-0 lg:text-7xl text-gray-200 text-center lg:text-start font-extrabold font-handlee">
+    //         Our Teams
+    //       </div>
+    //     </div>
+    //   </div>
 
-      <UnderConstruction/>
-      {/* <SeperatorHeading label="Ezyshop Founder"/>
-        <div className="flex items-center justify-center my-10 text-4xl font-bold text-cus dark:text-Green font-handlee">Meet Our Founder</div>
-      <TeamMember/>
+      
+      // <SeperatorHeading label="Ezyshop Founder"/>
+      //   <div className="flex items-center justify-center my-10 text-4xl font-bold text-cus dark:text-Green font-handlee">Meet Our Founder</div>
+      // <TeamMember/>
 
-      <SeperatorHeading label="Testimonials"/>
-      <div className="flex items-center justify-center my-10 text-4xl font-bold text-Green font-handlee">Customer Reviews!</div>
+      // <SeperatorHeading label="Testimonials"/>
+      // <div className="flex items-center justify-center my-10 text-4xl font-bold text-Green font-handlee">Customer Reviews!</div>
 
-      <ReviewsCarousel/> */}
-    </div>
+      // <ReviewsCarousel/>
+    // </div>
   );
 };
 
