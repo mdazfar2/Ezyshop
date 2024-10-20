738eec6c9d4cb106f7165b0b6cf8782de5753822
Shivansh <shivanshsaxena2910@gmail.com>
2024-10-20 22:13:45 +0530

    lint

--- app/(Seller)/(routes)/[storeId]/categories/components/cell-action.tsx
+++ app/(Seller)/(routes)/[storeId]/categories/components/cell-action.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { CategoryColumn } from "./columns";
 import { Button } from "@/components/ui/button";
-import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { Copy, MoreHorizontal, Trash } from "lucide-react";
 import toast from "react-hot-toast";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
