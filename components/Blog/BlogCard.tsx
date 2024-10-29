de9cd9a0ecbb9d01aca44b8b30c71b6cdbd149a7
D.V.Kavya Sai <d.v.kavyasai@gmail.com>
2024-10-29 21:13:23 +0530

    Improve UI of BlogCard to make it fit the screen size

--- components/Blog/BlogCard.tsx
+++ components/Blog/BlogCard.tsx
@@ -29,22 +29,22 @@ const BlogCard: React.FC<BlogData> = ({ blogData }) =>  {
 
   return (
     <div className="flex flex-col items-center justify-center gap-5 pb-10 h-full ">
-      <div className="h-full flex items-center justify-center flex-col gap-5 lg:grid grid-cols-3 lg:gap-10">
+      <div className="h-full flex items-center justify-center flex-col gap-5 lg:grid grid-cols-3 ">
         {blogData.map((card, index) => (
           <Card
             key={index}
-            className="w-[400px] hover:scale-105 transition duration-300 bg-gray-200 dark:bg-gray-700"
+            className="w-[350px] hover:scale-105 transition duration-300 bg-gray-200 dark:bg-gray-700"
           >
-            <CardHeader>
-              <div className="flex flex-col gap-2">
-                <Image alt="card image" width={1000} height={1000} src={card.image} />
+            <CardHeader className="p-0">
+              <div className="flex flex-col gap-2 p-0">
+                <Image alt="card image" className=" z-0 rounded-tl-lg rounded-tr-lg mb-0" width={800} height={800} src={card.image} />
 
-                <CardTitle className="flex text-center items-center text-2xl pt-4 text-customBlue dark:text-Green mt-2 justify-center font-handlee">
+                <CardTitle className="flex text-center items-center text-xl pt-2 px-3 text-customBlue dark:text-Green mt-2 justify-center font-handlee">
                   {card.title}
                 </CardTitle>
               </div>
-              <CardContent className="text-justify text-gray-500 text-md">
-                <div className="flex items-center justify-center gap-4 mb-4">
+              <CardContent className="text-justify text-gray-500 text-md md:px-3  md:pt-2 md:pb-0">
+                <div className="flex items-center justify-center gap-4 mb-2">
                   <div className="flex items-center justify-center gap-2">
                     <User className="h-4 w-4 text-customTeal dark:text-Yellow" />
                     <div className="text-sm text-gray-500"> {card.author}</div>
@@ -58,10 +58,10 @@ const BlogCard: React.FC<BlogData> = ({ blogData }) =>  {
                     <div className="text-sm text-gray-500">{ 22 }</div>
                   </div>
                 </div>
-                <div className="text-center dark:text-gray-200">{card.metaDescription}</div>
+                <div className="text-center dark:text-gray-200 text-md">{card.metaDescription}</div>
               </CardContent>
             </CardHeader>
-            <CardFooter className="flex justify-center">
+            <CardFooter className="flex justify-center md:pb-4 md:pt-3">
               <Link href={`/Blog/${card.id}`}>
                 <Button
                   variant="default"
