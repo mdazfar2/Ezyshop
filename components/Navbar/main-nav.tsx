0c19e4c0acad44e7f018c7a54432487c1adb5803
Priyanshu Verma <priyanshuverma@outlook.in>
2024-11-01 14:16:06 +0000

    feat: Navbar links accessable when logged in (Customer)

--- components/Navbar/main-nav.tsx
+++ components/Navbar/main-nav.tsx
@@ -10,26 +10,27 @@ import { Menu, X } from "lucide-react"; // Icons for hamburger menu
 import { ModeToggle } from "../ui/themeButton";
 import AuthButtons from "./authButtons";
 import { useFlashAlert } from "@/context/flashAlertContext";
+import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
 // import { useConstruction } from "@/context/modalContext";
 
-interface MainNavProps{
-  className?:React.HTMLAttributes<HTMLElement>
-  theme:string
+interface MainNavProps {
+  className?: React.HTMLAttributes<HTMLElement>;
+  theme: string;
 }
 
-
-export function MainNav({ className,theme }:MainNavProps) {
-
-  const [loading,setLoading]=useState(true);
-  const {openDialog}=useFlashAlert();
+export function MainNav({ className, theme }: MainNavProps) {
+  const [loading, setLoading] = useState(true);
+  const { openDialog } = useFlashAlert();
   const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false); // State to toggle mobile menu visibility
+  const session = useSession();
 
-  useEffect(()=>{
+  useEffect(() => {
     setLoading(false);
-  },[])
+  }, []);
 
-  if(loading) return null;
+  if (loading) return null;
 
   const toggleMenu = () => setIsOpen(!isOpen); // Toggle function for menu
 
@@ -42,29 +43,62 @@ export function MainNav({ className,theme }:MainNavProps) {
       label: "Categories",
       active: pathname.startsWith(`/Categories`),
     },
-    { href: `#`, label: "Teams", active: pathname.startsWith(`/Teams`),
+    {
+      href: `#`,
+      label: "Teams",
+      active: pathname.startsWith(`/Teams`),
       onClick: (e: React.MouseEvent) => {
         e.preventDefault();
-        openDialog(); 
+        openDialog();
       },
-     },
+    },
     { href: `/Blog`, label: "Blog", active: pathname.startsWith(`/Blog`) },
     {
-      href: `/MyOrders`,
+      href: session.status == "unauthenticated" ? "#" : `/MyOrders`,
       label: "My Orders",
       active: pathname.startsWith(`/MyOrders`),
+      onClick: (e: React.MouseEvent) => {
+        e.preventDefault();
+        if (session.status == "unauthenticated") {
+          toast.error("Please login as a Customer first");
+        }
+      },
     },
     {
-      href: `/WishList`,
+      href: session.status == "unauthenticated" ? "#" : `/WishList`,
       label: "Wish List",
       active: pathname.startsWith(`/WishList`),
-      logo: <Heart className={`h-5 w-5 ${theme==="dark"?`text-Green`:`text-customTeal`}`} />,
+      logo: (
+        <Heart
+          className={`h-5 w-5 ${
+            theme === "dark" ? `text-Green` : `text-customTeal`
+          }`}
+        />
+      ),
+      onClick: (e: React.MouseEvent) => {
+        e.preventDefault();
+        if (session.status == "unauthenticated") {
+          toast.error("Please login as a Customer first");
+        }
+      },
     },
     {
-      href: `/Cart`,
+      href: session.status == "unauthenticated" ? "#" : `/Cart`,
       label: "Cart",
       active: pathname.startsWith(`/Cart`),
-      logo: <ShoppingCart className={`h-5 w-5 ${theme==="dark"?`text-Green`:`text-customTeal`}`} />,
+      logo: (
+        <ShoppingCart
+          className={`h-5 w-5 ${
+            theme === "dark" ? `text-Green` : `text-customTeal`
+          }`}
+        />
+      ),
+      onClick: (e: React.MouseEvent) => {
+        e.preventDefault();
+        if (session.status == "unauthenticated") {
+          toast.error("Please login as a Customer first");
+        }
+      },
     },
     {
       href: `/Contact`,
@@ -77,25 +111,30 @@ export function MainNav({ className,theme }:MainNavProps) {
     <nav className={cn("flex items-center justify-between", className)}>
       {/* Hamburger icon for mobile */}
       <div className="lg:hidden flex items-center justify-center gap-2">
-        <ModeToggle/>
-        <button
-          onClick={toggleMenu}
-          className="text-White focus:outline-none"
-        >
+        <ModeToggle />
+        <button onClick={toggleMenu} className="text-White focus:outline-none">
           {isOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
       </div>
 
       {/* Navigation links (shown on larger screens, hidden on mobile) */}
-      <div className={cn("hidden lg:flex items-center justify-centerx space-x-4 lg:space-x-6")}>
+      <div
+        className={cn(
+          "hidden lg:flex items-center justify-centerx space-x-4 lg:space-x-6"
+        )}
+      >
         {routes.map((route) => (
           <Link
             key={route.href}
             href={route.href}
             className={cn(
               "font-nunito flex items-center gap-2 justify-center font-extrabold text-lg",
-              route.active ?`${theme==='dark'?'text-gray-500':'text-customTeal'}`:`${theme==='dark'?'text-gray-200':'text-customBlue'}`,
-              theme=='dark'? `${'hover:text-gray-500'}`:`${'hover:text-customTeal'}`
+              route.active
+                ? `${theme === "dark" ? "text-gray-500" : "text-customTeal"}`
+                : `${theme === "dark" ? "text-gray-200" : "text-customBlue"}`,
+              theme == "dark"
+                ? `${"hover:text-gray-500"}`
+                : `${"hover:text-customTeal"}`
             )}
             onClick={route.onClick}
           >
@@ -103,10 +142,10 @@ export function MainNav({ className,theme }:MainNavProps) {
             {route.label}
           </Link>
         ))}
-        
-        <ModeToggle/>
 
-        <AuthButtons toggleMenu={toggleMenu}/>
+        <ModeToggle />
+
+        <AuthButtons toggleMenu={toggleMenu} />
       </div>
 
       {/* Mobile menu with slow opening animation */}
@@ -124,20 +163,24 @@ export function MainNav({ className,theme }:MainNavProps) {
             href={route.href}
             className={cn(
               "font-nunito flex items-center gap-2 justify-center font-extrabold text-lg",
-              route.active ?`${theme==='dark'?'text-gray-200':'text-customTeal'}`:`${theme==='dark'?'text-gray-200':'text-customBlue'}`,
-              theme=='dark'? `${'hover:text-gray-500'}`:`${'hover:text-customTeal'}`
+              route.active
+                ? `${theme === "dark" ? "text-gray-200" : "text-customTeal"}`
+                : `${theme === "dark" ? "text-gray-200" : "text-customBlue"}`,
+              theme == "dark"
+                ? `${"hover:text-gray-500"}`
+                : `${"hover:text-customTeal"}`
             )}
             onClick={(e) => {
               toggleMenu(); // Close menu on link click
               if (route.onClick) route.onClick(e); // Call route's onClick if it exists
-            }}// Close menu on link click
+            }} // Close menu on link click
           >
             {route.logo && <span>{route.logo}</span>}
             {route.label}
           </Link>
         ))}
 
-        <AuthButtons toggleMenu={toggleMenu}/>
+        <AuthButtons toggleMenu={toggleMenu} />
       </div>
     </nav>
   );
