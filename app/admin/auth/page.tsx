a3c5bdd2ac291f6c698548b853361ba1167f2e0c
Shivansh <shivanshsaxena2910@gmail.com>
2024-11-07 08:47:37 +0530

    error

--- app/admin/auth/page.tsx
+++ app/admin/auth/page.tsx
@@ -1,9 +1,5 @@
 import { Metadata } from 'next';
 import Image from 'next/image';
-import Link from 'next/link';
-
-import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
 import { AdminLoginForm } from '@/app/admin/auth/components/login-form';
 
 export const metadata: Metadata = {
