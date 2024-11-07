c7e24131bb3e8068469f07ef5913637bd0404fed
Shivansh <shivanshsaxena2910@gmail.com>
2024-11-07 07:26:43 +0530

    new

--- app/admin/auth/components/login-form.tsx
+++ app/admin/auth/components/login-form.tsx
@@ -8,8 +8,6 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import toast, { Toaster } from 'react-hot-toast';
 import { Spinner } from '@/components/ui/spinner';
-import { OtpForm } from './otp-form';
-import { AdminVerify } from '@/actions/Admin/login-admin';
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {
   authType: 'signup' | 'login';
@@ -24,36 +22,12 @@ export function UserLoginForm({
   const [email, setEmail] = React.useState('');
   const [otpOpen, setOtpOpen] = React.useState(false);
 
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault();
-    setIsLoading(true);
-
-    if (!email) {
-      toast.error('missing details');
-      setIsLoading(false);
-      return;
-    }
-    const res = await AdminVerify({
-      email: email,
-    });
-
-    if (!res.success && res.error) {
-      toast.error(res.error);
-      setIsLoading(false);
-      return;
-    }
-
-    toast.success('user is valid, please enter OTP');
-
-    setOtpOpen(true);
-    setIsLoading(false);
-  }
-
+  
   return (
     <div className={cn('grid gap-6', className)} {...props}>
       <Toaster />
       {!otpOpen && (
-        <form onSubmit={onSubmit}>
+        <form >
           <div className="grid gap-2">
             <div className="grid gap-1">
               <Label className="sr-only" htmlFor="email">
@@ -85,9 +59,6 @@ export function UserLoginForm({
             </Button>
           </div>
         </form>
-      )}
-      {otpOpen && (
-        <OtpForm email={email} setOtpOpen={setOtpOpen} roleType="admin" />
       )}
     </div>
   );
